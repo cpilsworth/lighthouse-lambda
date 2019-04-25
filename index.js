3585a3f4656ce2fd0c0f32848f8ef4e81c40f82b
Chris Pilsworth <cpilsworth@users.noreply.github.com>
2019-04-25 16:14:34 +0100

    async/await version

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-/*jshint esversion: 6 */
+/*jshint esversion: 8 */
 const createLighthouse = require('lighthouse-lambda');
+const AWS = require('aws-sdk');
 
-exports.handler = function (event, context, callback) {
-  Promise.resolve()
-    .then(() => createLighthouse(process.env.URL, { logLevel: 'info' }))
-    .then(({ chrome, start }) => {
-      return start()
-        .then((results) => {
-          console.log(JSON.stringify(results));
-          return chrome.kill().then(() => callback(null));
-        })
-        .catch((error) => {
-          console.log(error);
-          // Handle errors when running Lighthouse
-          return chrome.kill().then(() => callback(error));
-        });
-    })
-    // Handle other errors
-    .catch(callback);
+exports.handler = async function (event, context) {
+  
+  let { chrome, start } = await createLighthouse(process.env.URL, { logLevel: 'info' });
+  let results = await start();
+
+  console.log(JSON.stringify(results.lhr));
+
+  const s3 = new AWS.S3();
+  var params = {
+    Bucket : process.env.BUCKET,
+    Key : process.env.KEY,
+    Body : JSON.stringify(results.lhr)
+  };
+  await s3.putObject(params).promise();
+
+  await chrome.kill();
+
 };
