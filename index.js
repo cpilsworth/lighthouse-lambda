37cbedc4b642628179e737119722f34f3641cc65
Chris Pilsworth <christopher.pilsworth@hsbc.com>
2019-04-25 11:20:15 +0100

    Return results

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-/*jshint esversion: 8 */
+/*jshint esversion: 6 */
 const createLighthouse = require('lighthouse-lambda');
 
-exports.handler = async function (event, context) {
+exports.handler = function (event, context, callback) {
   Promise.resolve()
     .then(() => createLighthouse(process.env.URL, { logLevel: 'info' }))
     .then(({ chrome, start }) => {
       return start()
         .then((results) => {
-          console.log( JSON.stringify(results));
-          return chrome.kill().then(() => JSON.stringify(results));
+          console.log(JSON.stringify(results));
+          return chrome.kill().then(() => callback(JSON.stringify(results)));
         })
         .catch((error) => {
           console.log(error);
           // Handle errors when running Lighthouse
-          return chrome.kill().then(() => error);
+          return chrome.kill().then(() => callback(error));
         });
-    });
+    })
+    // Handle other errors
+    .catch(callback);
 };
