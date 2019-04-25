3a1d0167d91466aee2ec67304e2127f853bcf824
Chris Pilsworth <christopher.pilsworth@hsbc.com>
2019-04-25 09:55:52 +0100

    Added script to lighthouse a URL
    URL is specified as environment variable

---
+++ index.js
@@ -0,0 +1,19 @@
+const createLighthouse = require('lighthouse-lambda')
+
+exports.handler = function (event, context, callback) {
+  Promise.resolve()
+    .then(() => createLighthouse(process.env.URL', { logLevel: 'info' }))
+    .then(({ chrome, start }) => {
+      return start()
+        .then((results) => {
+          // Do something with `results`
+          return chrome.kill().then(() => callback(null))
+        })
+        .catch((error) => {
+          // Handle errors when running Lighthouse
+          return chrome.kill().then(() => callback(error))
+        })
+    })
+    // Handle other errors
+    .catch(callback)
+}
