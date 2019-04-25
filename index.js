668e186a2a407081d17a5ccc5436b9e058b4400e
Chris Pilsworth <christopher.pilsworth@hsbc.com>
2019-04-25 10:52:24 +0100

    Async handler with results returned

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-/*jshint esversion: 6 */
+/*jshint esversion: 8 */
 const createLighthouse = require('lighthouse-lambda');
 
-exports.handler = function (event, context, callback) {
+exports.handler = async function (event, context) {
   Promise.resolve()
     .then(() => createLighthouse(process.env.URL, { logLevel: 'info' }))
     .then(({ chrome, start }) => {
       return start()
         .then((results) => {
-          console.log(JSON.stringify(results));
-          return chrome.kill().then(() => callback(null));
+          return chrome.kill().then(() => JSON.stringify(results));
         })
         .catch((error) => {
-          console.log(error);
           // Handle errors when running Lighthouse
-          return chrome.kill().then(() => callback(error));
+          return chrome.kill().then(() => error);
         });
     })
     // Handle other errors
